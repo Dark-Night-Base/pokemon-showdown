9c17ef6368098919c0ad19974a0b780111885e9c
Nihilslave <37762803+Nihilslave@users.noreply.github.com>
2022-05-25 20:43:01 +0800

    Update saveserverreplay.ts

--- server/chat-plugins/saveserverreplay.ts
+++ server/chat-plugins/saveserverreplay.ts
@@ -8,20 +8,22 @@ export const commands: Chat.ChatCommands = {
 	saveserverreplay(target, room) {
 		// function saveReplay in /test/common.js
 		if (!room) {
-			return;
+			return this.errorReply(this.tr`You can only save replays for battles.`);
 		}
 		if (room.type == 'battle') {
 			const battleLog = room.getLog(0);
-			const filePath = path.resolve(__dirname, `../../replays/${room.roomid}.html`);//-${Date.now()}.html`);
-			const out = fs.createWriteStream(filePath, {flags: 'a'});
+			const filePath = path.resolve(__dirname, `../../replays/${room.roomid}-${room.p1}-${room.p2}.html`);//-${Date.now()}.html`);
+			const out = fs.createWriteStream(filePath, {flags: 'w'});
 			out.on('open', () => {
 				out.write(
 					`<!DOCTYPE html>\n` +
+					`<meta charset="utf-8" />\n` +
 					`<script type="text/plain" class="battle-log-data">${battleLog}</script>\n` +
 					`<script src="https://play.pokemonshowdown.com/js/replay-embed.js"></script>\n`
 				);
 				out.end();
 			});
+			return this.errorReply(this.tr`Your replay has been uploaded! It's available at: http://replay.sciroccogti.top/files/${room.roomid}-${room.p1}-${room.p2}.html`);
 		}
 	},
 	saveserverreplayhelp: ["/ssr or /saveserverreplay - Save the replay of the battle to server. (We are not a registered server so saving replays to replay.pokemonshowdown.com won't work.)",],
