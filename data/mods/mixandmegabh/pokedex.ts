cf6b301786527fb8244be20c56466e108481ca8a
Nihilslave <ApclpsZm90@hotmail.com>
2022-07-27 11:40:35 +0800

    server

---
+++ data/mods/mixandmegabh/pokedex.ts
@@ -0,0 +1,22 @@
+export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+	calyrexice: {
+		inherit: true, 
+		baseSpecies: "Glastrier",
+		requiredMove: "Glacial Lance",
+		forme: "Mega",
+	},
+	calyrexshadow: {
+		inherit: true, 
+		baseSpecies: "Spectrier",
+		requiredMove: "Astral Barrage",
+		forme: "Mega",
+	},
+	zaciancrowned: {
+		inherit: true,
+		forme: "Primal", 
+	},
+	zamazentacrowned: {
+		inherit: true,
+		forme: "Primal",
+	},
+};
