80b0e49aabc67bdb1f95402fedb47229813224ef
Nihilslave <ApclpsZm90@hotmail.com>
2023-01-07 20:37:42 +0800

    mbhv4 - fix spin out

--- data/mods/gen9morebalancedhackmons/moves.ts
+++ data/mods/gen9morebalancedhackmons/moves.ts
@@ -181,6 +181,8 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		inherit: true,
 		basePower: 60,
 		pp: 20,
+		self: undefined,
+		secondary: undefined,
 		selfSwitch: true,
 		desc: "If this move is successful and the user has not fainted, the user switches out even if it is trapped and is replaced immediately by a selected party member. The user does not switch out if there are no unfainted party members, or if the target switched out using an Eject Button or through the effect of the Emergency Exit or Wimp Out Abilities.",
 		shortDesc: "User switches out after damaging the target.",
