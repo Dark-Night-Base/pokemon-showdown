cff894618ece3e77bb73edde7dec53b93f91f7d9
Nihilslave <ApclpsZm90@hotmail.com>
2023-02-17 13:31:31 +0800

    7ffa - remove nickname clause

--- config/formats.ts
+++ config/formats.ts
@@ -4899,7 +4899,7 @@ export const Formats: FormatList = [
 		gameType: 'freeforall',
 		rated: false,
 		tournamentShow: false,
-		ruleset: ['Obtainable', 'Team Preview', 'Nickname Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod'],
+		ruleset: ['Obtainable', 'Team Preview', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod'],
 		banlist: [
 			'Aromatic Mist', 'Final Gambit', 'Flatter', 'Flower Shield', 'Follow Me', 'Gear Up', 'Magnetic Flux',
 			'Rage Powder', 'Rototiller', 'Spotlight', 'Swagger',
