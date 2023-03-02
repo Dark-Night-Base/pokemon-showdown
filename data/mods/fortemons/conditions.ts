edaf4475da9b86d581810fa686f0bb4e2c8f257a
Nihilslave <ApclpsZm90@hotmail.com>
2023-03-02 19:27:54 +0800

    fortebh - create mod and really fix hydrosteam

---
+++ data/mods/fortemons/conditions.ts
@@ -0,0 +1,29 @@
+export const Conditions: {[k: string]: ModdedConditionData} = {
+	sunnyday: {
+		inherit: true,
+		onWeatherModifyDamage(damage, attacker, defender, move) {
+			if (move.id === 'hydrosteam' && !attacker.hasItem('utilityumbrella')) {
+				this.debug('Sunny Day Hydro Steam boost');
+				return this.chainModify(1.5);
+			}
+			// cannot use hasItem() here, see scripts.ts and u'll know why
+			if (attacker.getItem().id === 'hydrosteam') {
+				this.debug('Sunny Day Hydro Steam boost');
+				if (move.type === 'Fire' && !defender.hasItem('utilityumbrella')) {
+					this.debug('Sunny Day fire boost');
+					return this.chainModify(2.25);
+				}
+				return this.chainModify(1.5);
+			}
+			if (defender.hasItem('utilityumbrella')) return;
+			if (move.type === 'Fire') {
+				this.debug('Sunny Day fire boost');
+				return this.chainModify(1.5);
+			}
+			if (move.type === 'Water') {
+				this.debug('Sunny Day water suppress');
+				return this.chainModify(0.5);
+			}
+		},
+	},
+};
