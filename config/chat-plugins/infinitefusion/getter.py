5c15f1abd5ba9f63d95adcb6ac3cda185ccff37f
Nihilslave <ApclpsZm90@hotmail.com>
2023-04-28 13:32:32 +0800

    /ifget - just don't use \n

--- config/chat-plugins/infinitefusion/getter.py
+++ config/chat-plugins/infinitefusion/getter.py
@@ -6,9 +6,9 @@ while True:
     files = os.listdir('.')
     if 'done' in files:
         with open('task') as t:
-            lines = t.readlines()
+            lines = t.read().split(',')
             for line in lines:
                 args = line.split(' ')
-                subprocess.run(['python3', 'getRemoteIFSprite.py', args[0], args[1].replace('\n', '')], cwd='/home/mc/pokemon-showdown-client/sprites')
+                subprocess.run(['python3', 'getRemoteIFSprite.py', args[0], args[1]], cwd='/home/mc/pokemon-showdown-client/sprites')
         os.remove(os.path.join('.', 'task'))
         os.remove(os.path.join('.', 'done'))
