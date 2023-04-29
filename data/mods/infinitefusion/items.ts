7b1d92c2d04ef468e74bf99ea3f0dcc7a7b9929b
Nihilslave <ApclpsZm90@hotmail.com>
2023-04-30 00:05:13 +0800

    if - fix eviolite

--- data/mods/infinitefusion/items.ts
+++ data/mods/infinitefusion/items.ts
@@ -45,6 +45,19 @@ export const Items: {[k: string]: ModdedItemData} = {
 			}
 		},
 	},
+	eviolite: {
+		inherit: true,
+		onModifyDef(def, pokemon) {
+			if (pokemon.m.headSpecies?.nfe || pokemon.m.bodySpecies?.nfe) {
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpD(spd, pokemon) {
+			if (pokemon.m.headSpecies?.nfe || pokemon.m.bodySpecies?.nfe) {
+				return this.chainModify(1.5);
+			}
+		},
+	},
 	griseouscore: {
 		inherit: true,
 		onBasePower(basePower, user, target, move) {
