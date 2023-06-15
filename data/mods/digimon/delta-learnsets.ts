cfd6cf263419c84a8d71f139dd17ec7d05a5a87e
Nihilslave <ApclpsZm90@hotmail.com>
2023-06-15 10:12:13 +0800

    digimon - add delta-learnsets.ts

--- data/mods/digimon/delta-learnsets.ts
+++ data/mods/digimon/delta-learnsets.ts
@@ -1,1 +1,14 @@
-export const deltaLearnsetTable = {};
+interface deltaLearnsets {
+	[k: string]: ["9L1"] | 1, // 1 for types
+}
+interface deltaLearnsetData {
+	adds?: deltaLearnsets,
+	deletes?: deltaLearnsets,
+}
+
+export const deltaLearnsetTable: {[k: string]: deltaLearnsetData} = {
+	omegamon: {
+		adds: {},
+		deletes: {},
+	},
+};
