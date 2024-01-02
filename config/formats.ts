d174c64dea6c718d579951f2242b8db05f77f6db
Nihilslave <ApclpsZm90@hotmail.com>
2024-01-02 14:31:51 +0800

    lu

--- config/formats.ts
+++ config/formats.ts
@@ -314,25 +314,25 @@ export const Formats: FormatList = [
 					(this.terastallized || pokemon.terastallized))) {
 					return false;
 				}
-		
+
 				if (this.battle.dex.currentMod === 'gen1stadium' && (
 					species.name === 'Ditto' ||
 					(this.species.name === 'Ditto' && pokemon.moves.includes('transform'))
 				)) {
 					return false;
 				}
-		
+
 				if (!this.setSpecies(species, effect, true)) return false;
-		
+
 				this.transformed = true;
 				this.weighthg = pokemon.weighthg;
-		
+
 				const types = pokemon.getTypes(true, true);
 				this.setType(pokemon.volatiles['roost'] ? pokemon.volatiles['roost'].typeWas : types, true);
 				this.addedType = pokemon.addedType;
 				this.knownType = this.isAlly(pokemon) && pokemon.knownType;
 				this.apparentType = pokemon.apparentType;
-		
+
 				let statName: StatIDExceptHP;
 				for (statName in this.storedStats) {
 					this.storedStats[statName] = pokemon.storedStats[statName];
@@ -397,7 +397,7 @@ export const Formats: FormatList = [
 						}
 					}
 				}
-		
+
 				// Change formes based on held items (for Transform)
 				// Only ever relevant in Generation 4 since Generation 3 didn't have item-based forme changes
 				if (this.battle.gen === 4) {
@@ -418,12 +418,12 @@ export const Formats: FormatList = [
 						}
 					}
 				}
-		
+
 				// Pokemon transformed into Ogerpon cannot Terastallize
 				// restoring their ability to tera after they untransform is handled ELSEWHERE
 				if (this.species.baseSpecies === 'Ogerpon' && this.canTerastallize) this.canTerastallize = false;
 				if (this.species.baseSpecies === 'Terapagos' && this.canTerastallize) this.canTerastallize = false;
-		
+
 				return true;
 			},
 		},
