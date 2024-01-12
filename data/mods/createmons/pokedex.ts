9b6fba08334f0ed71d9fd594c1f76519b61f8042
Nihilslave <ApclpsZm90@hotmail.com>
2024-01-12 16:39:40 +0800

    bc - disable forceTeraType

---
+++ data/mods/createmons/pokedex.ts
@@ -0,0 +1,46 @@
+export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+	ogerpon: {
+		inherit: true,
+		forceTeraType: undefined,
+	},
+	ogerponwellspring: {
+		inherit: true,
+		forceTeraType: undefined,
+	},
+	ogerponhearthflame: {
+		inherit: true,
+		forceTeraType: undefined,
+	},
+	ogerponcornerstone: {
+		inherit: true,
+		forceTeraType: undefined,
+	},
+	ogerpontealtera: {
+		inherit: true,
+		forceTeraType: undefined,
+	},
+	ogerponwellspringtera: {
+		inherit: true,
+		forceTeraType: undefined,
+	},
+	ogerponhearthflametera: {
+		inherit: true,
+		forceTeraType: undefined,
+	},
+	ogerponcornerstonetera: {
+		inherit: true,
+		forceTeraType: undefined,
+	},
+	terapagos: {
+		inherit: true,
+		forceTeraType: undefined,
+	},
+	terapagosterastal: {
+		inherit: true,
+		forceTeraType: undefined,
+	},
+	terapagosstellar: {
+		inherit: true,
+		forceTeraType: undefined,
+	},
+};
