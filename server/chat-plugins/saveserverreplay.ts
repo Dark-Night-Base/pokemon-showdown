69ebb3dc6ca493025031f1a42b7c898664abcebb
Nihilslave <ApclpsZm90@hotmail.com>
2025-06-19 23:43:07 +0800

    ffa - anonymous replays 2

--- server/chat-plugins/saveserverreplay.ts
+++ server/chat-plugins/saveserverreplay.ts
@@ -12,6 +12,9 @@ export const commands: Chat.ChatCommands = {
 		}
 
 		const format = Dex.formats.get(room.battle.format, true);
+		const isFFA = room.battle.gameType === 'freeforall';
+		const p1 = isFFA ? 'p1' : room.p1?.name;
+		const p2 = isFFA ? 'p2' : room.p2?.name;
 		let hideDetails = !format.id.includes('customgame');
 		if (format.team && room.battle.ended) hideDetails = false;
 		const data = room.getLog(hideDetails ? 0 : -1);
@@ -21,7 +24,7 @@ export const commands: Chat.ChatCommands = {
 			hideDetails = false;
 		}
 		let link = `${room.roomid.slice(7)}-${room.p1}-${room.p2}.html`;
-		if (room.battle.gameType === 'freeforall') link = `${room.roomid.slice(7)}.html`;
+		if (isFFA) link = `${room.roomid.slice(7)}.html`;
 		let rating = 0;
 		if (room.battle.ended && room.battle.rated) rating = room.battle.rated;
 		const secret = room.settings.isPrivate || room.hideReplay;
@@ -41,8 +44,8 @@ export const commands: Chat.ChatCommands = {
 			out.write(
 				`<!DOCTYPE html>\n` +
 				`<meta charset="utf-8" />\n` +
-				`<title>${format} replay: ${room.p1?.name} vs. ${room.p2?.name}</title>\n` +
-				`<meta name="description" content="Watch a replay of a Pokémon battle between ${room.p1?.name} and ${room.p2?.name} (${format})" />` +
+				`<title>${format} replay: ${p1} vs. ${p2}</title>\n` +
+				`<meta name="description" content="Watch a replay of a Pokémon battle between ${p1} and ${p2} (${format})" />` +
 				'<style>\n' +
 				'html,body {font-family:Verdana, sans-serif;font-size:10pt;margin:0;padding:0;}body{padding:12px 0;} .battle-log {font-family:Verdana, sans-serif;font-size:10pt;} .battle-log-inline {border:1px solid #AAAAAA;background:#EEF2F5;color:black;max-width:640px;margin:0 auto 80px;padding-bottom:5px;} .battle-log .inner {padding:4px 8px 0px 8px;} .battle-log .inner-preempt {padding:0 8px 4px 8px;} .battle-log .inner-after {margin-top:0.5em;} .battle-log h2 {margin:0.5em -8px;padding:4px 8px;border:1px solid #AAAAAA;background:#E0E7EA;border-left:0;border-right:0;font-family:Verdana, sans-serif;font-size:13pt;} .battle-log .chat {vertical-align:middle;padding:3px 0 3px 0;font-size:8pt;} .battle-log .chat strong {color:#40576A;} .battle-log .chat em {padding:1px 4px 1px 3px;color:#000000;font-style:normal;} .chat.mine {background:rgba(0,0,0,0.05);margin-left:-8px;margin-right:-8px;padding-left:8px;padding-right:8px;} .spoiler {color:#BBBBBB;background:#BBBBBB;padding:0px 3px;} .spoiler:hover, .spoiler:active, .spoiler-shown {color:#000000;background:#E2E2E2;padding:0px 3px;} .spoiler a {color:#BBBBBB;} .spoiler:hover a, .spoiler:active a, .spoiler-shown a {color:#2288CC;} .chat code, .chat .spoiler:hover code, .chat .spoiler:active code, .chat .spoiler-shown code {border:1px solid #C0C0C0;background:#EEEEEE;color:black;padding:0 2px;} .chat .spoiler code {border:1px solid #CCCCCC;background:#CCCCCC;color:#CCCCCC;} .battle-log .rated {padding:3px 4px;} .battle-log .rated strong {color:white;background:#89A;padding:1px 4px;border-radius:4px;} .spacer {margin-top:0.5em;} .message-announce {background:#6688AA;color:white;padding:1px 4px 2px;} .message-announce a, .broadcast-green a, .broadcast-blue a, .broadcast-red a {color:#DDEEFF;} .broadcast-green {background-color:#559955;color:white;padding:2px 4px;} .broadcast-blue {background-color:#6688AA;color:white;padding:2px 4px;} .infobox {border:1px solid #6688AA;padding:2px 4px;} .infobox-limited {max-height:200px;overflow:auto;overflow-x:hidden;} .broadcast-red {background-color:#AA5544;color:white;padding:2px 4px;} .message-learn-canlearn {font-weight:bold;color:#228822;text-decoration:underline;} .message-learn-cannotlearn {font-weight:bold;color:#CC2222;text-decoration:underline;} .message-effect-weak {font-weight:bold;color:#CC2222;} .message-effect-resist {font-weight:bold;color:#6688AA;} .message-effect-immune {font-weight:bold;color:#666666;} .message-learn-list {margin-top:0;margin-bottom:0;} .message-throttle-notice, .message-error {color:#992222;} .message-overflow, .chat small.message-overflow {font-size:0pt;} .message-overflow::before {font-size:9pt;content:\'...\';} .subtle {color:#3A4A66;}\n' +
 				'</style>\n' +
@@ -52,7 +55,7 @@ export const commands: Chat.ChatCommands = {
 				// maybe use room.battle.stream?
 				'<div class="battle"></div><div class="battle-log"></div><div class="replay-controls"></div><div class="replay-controls-2"></div>\n' +
 				`<pre class="urlbox" style="word-wrap: break-word;">https://replay.sciroccogti.top/files/${link}</pre>\n` +
-				`<h1 style="font-weight:normal;text-align:left"><strong>${format}</strong>: <a href="https://pokemonshowdown.com/users/${toID(room.p1?.name)}" class="subtle" target="_blank">${room.p1?.name}</a> vs. <a href="https://pokemonshowdown.com/users/${toID(room.p2?.name)}" class="subtle" target="_blank">${room.p2?.name}</a></h1>\n` +
+				`<h1 style="font-weight:normal;text-align:left"><strong>${format}</strong>: <a href="https://pokemonshowdown.com/users/${toID(p1)}" class="subtle" target="_blank">${p1}</a> vs. <a href="https://pokemonshowdown.com/users/${toID(p2)}" class="subtle" target="_blank">${p2}</a></h1>\n` +
 				'<p style="padding:0 1em;margin-top:0">' +
 				`<small class="uploaddate" data-timestamp="${Date.now() / 1000}"><em>Uploaded:</em> ${new Date().toDateString().split(" ")[1]} ${new Date().toDateString().split(" ")[2]}, ${new Date().getFullYear()} ${rating ? `| <em>Rating:</em>${rating}` : ''}</small>` +
 				'</p>' +
